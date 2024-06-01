e91725c910a2cfc7fdc4200d6b6bcd1a1696087f
Jukie1 <155185285+Jukie-1@users.noreply.github.com>
2024-06-01 16:11:11 -0400

    Add files via upload

---
+++ obj/Release/netstandard2/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace AtomicLoader
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "AtomicLoader";
+        public const string PLUGIN_NAME = "AtomicLoader";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
